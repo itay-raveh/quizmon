46aac9f1dc37640c32b0ebc1ec33cf121c97aa69
Itay Raveh <itayitay30@gmail.com>
2022-10-04 16:02:20 +0300

    ✨ Add `QueryClientProvider` and `ReactQueryDevtools`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
 import { MantineProvider } from "@mantine/core";
 import { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const qc = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <QueryClientProvider client={qc}>
       <MantineProvider withGlobalStyles withNormalizeCSS>
         <Component {...pageProps} />
       </MantineProvider>
-    </>
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
   );
 };
 
