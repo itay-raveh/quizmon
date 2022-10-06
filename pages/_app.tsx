cfe04c08bb543929d750668a9770adeab1698b2b
Itay Raveh <itayitay30@gmail.com>
2022-10-06 03:02:19 +0300

    feat(styling): ✨ add mantine color scheme context and preference cookie

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,71 @@
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from '@mantine/core';
+import { useColorScheme } from '@mantine/hooks';
+import { getCookie, setCookie } from 'cookies-next';
+import {
+  AppContext,
+  AppProps as NextAppProps,
+  default as NextApp,
+} from 'next/app';
 import { MantineProvider } from '@mantine/core';
 import { AppProps } from 'next/app';
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
+const COLOR_SCHEME_COOKIE = 'color-scheme';
+
 const qc = new QueryClient();
 
-const App = ({ Component, pageProps }: AppProps) => {
+interface AppProps extends NextAppProps {
+  colorSchemeCookie?: ColorScheme;
+}
+
+const App = ({ Component, pageProps, colorSchemeCookie }: AppProps) => {
+  // get the system color scheme preference
+  const preferredColorScheme = useColorScheme();
+
+  // initialize color scheme state with cookie, fallback to system preference
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    colorSchemeCookie || preferredColorScheme
+  );
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme =
+      value || (colorScheme === 'dark' ? 'light' : 'dark');
+    setColorScheme(nextColorScheme);
+    setCookie(COLOR_SCHEME_COOKIE, nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30, // 30 days
+    });
+  };
+
   return (
     <QueryClientProvider client={qc}>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-        <Component {...pageProps} />
-      </MantineProvider>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
       {process.env.NODE_ENV === 'development' && (
         <ReactQueryDevtools initialIsOpen={false} />
       )}
     </QueryClientProvider>
   );
+};
+
+App.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await NextApp.getInitialProps(appContext);
+
+  return {
+    ...appProps,
+    colorSchemeCookie: getCookie(COLOR_SCHEME_COOKIE, appContext.ctx),
+  };
 };
 
 export default App;
