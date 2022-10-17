359af2d62216a6c8498420617671fa82a9ee7882
Itay Raveh <itayitay30@gmail.com>
2022-10-17 19:18:53 +0300

    feat(pages): ✨ add question pagination to `/`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,54 @@
-import { Button, Center, Code, Group, Stack } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+import { Button, Center, Group, Loader, Stack } from '@mantine/core';
+import { useCounter, useDisclosure } from '@mantine/hooks';
 import ModifiersFormModal from 'components/ModifiersFormModal';
+import Question from 'components/Question';
 import { modifiersInitialValues } from 'lib/models/Modifiers';
+import { pokeapi } from 'lib/pokeapi';
 import type { NextPage } from 'next';
 import Image from 'next/future/image';
 import logo from 'public/logo.png';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+import { useQueries } from 'react-query';
 
 const IndexPage: NextPage = () => {
   const [modifiers, setModifiers] = useState(modifiersInitialValues);
+
+  const [started, setStarted] = useState(false);
+
   const [opened, { close, open }] = useDisclosure(false);
+
+  const genQueriesResults = useQueries(
+    modifiers.generations.map((_, idx) => ({
+      queryKey: ['generation', idx + 1],
+      queryFn: () =>
+        pokeapi.game
+          .getGenerationById(idx + 1)
+          .then((gen) => gen.pokemon_species.map((species) => species.name)),
+    }))
+  );
+
+  const pokemonNameList = genQueriesResults.flatMap(
+    (genResult) => genResult.data
+  );
+
+  const questions = useMemo(
+    () =>
+      pokemonNameList.map((pokemonName) =>
+        pokemonName ? (
+          <Question
+            pokemonName={pokemonName}
+            pokemonNameList={pokemonNameList}
+            modifiers={modifiers}
+          />
+        ) : (
+          <Loader />
+        )
+      ),
+    [modifiers, pokemonNameList]
+  );
+
+  const [questionIdx, { increment: nextQuestion, decrement: prevQuestion }] =
+    useCounter(0, { min: 0, max: pokemonNameList.length - 1 });
 
   return (
     <main>
@@ -20,19 +59,30 @@ const IndexPage: NextPage = () => {
       />
       <Center sx={{ height: '100vh' }}>
         <Stack align='center'>
-          <Image
-            src={logo}
-            alt='Quizmon: The Ultimate Pokémon Knowledge Test'
-            style={{ maxWidth: '30rem', height: 'auto' }}
-            priority
-          />
-          <Group>
-            <Button variant='outline' onClick={open}>
-              Modifiers
-            </Button>
-            <Button>Start</Button>
-          </Group>
-          <Code block>{JSON.stringify(modifiers, undefined, 2)}</Code>
+          {!started ? (
+            <>
+              <Image
+                src={logo}
+                alt='Quizmon: The Ultimate Pokémon Knowledge Test'
+                style={{ maxWidth: '30rem', height: 'auto' }}
+                priority
+              />
+              <Group>
+                <Button variant='outline' onClick={open}>
+                  Modifiers
+                </Button>
+                <Button onClick={() => setStarted(true)}>Start</Button>
+              </Group>
+            </>
+          ) : (
+            <>
+              {questions[questionIdx]}
+              <Group>
+                <Button onClick={prevQuestion}>Back</Button>
+                <Button onClick={nextQuestion}>Next</Button>
+              </Group>
+            </>
+          )}
         </Stack>
       </Center>
       <footer>
