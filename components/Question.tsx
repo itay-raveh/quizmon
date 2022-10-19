9d50fc0f2d30241b0362f1ea8dd6b50f51db8ef2
Itay Raveh <itayitay30@gmail.com>
2022-10-19 23:47:11 +0300

    feat(components): ✨ color options on click in `Question`

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,7 +1,7 @@
 import { Button, Center, Group, Loader, Stack } from '@mantine/core';
 import { pokeapi } from 'lib/pokeapi';
 import startCase from 'lodash.startcase';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useQuery } from 'react-query';
 import Sprite, { SPRITE_SIZE } from './Sprite';
 
@@ -17,6 +17,8 @@ const Question: FC<QuestionProps> = ({ pokemonFormName, options }) => {
     () => pokeapi.pokemon.getPokemonFormByName(pokemonFormName)
   );
 
+  const [clickedOption, setClickedOption] = useState('');
+
   if (isLoading)
     return (
       <Center sx={{ width: SPRITE_SIZE + 50, height: SPRITE_SIZE + 50 }}>
@@ -26,13 +28,39 @@ const Question: FC<QuestionProps> = ({ pokemonFormName, options }) => {
 
   if (!pokemonForm) return <section>No such pokemon</section>;
 
+  const getColor = (option: string) => {
+    // if no button was clicked yet
+    if (!clickedOption)
+      // all buttons are normal
+      // (undefined means default from theme)
+      return undefined;
+
+    // if this is the correct option
+    if (option === pokemonFormName)
+      // mark it as correct
+      return 'green';
+
+    // if this is NOT the correct option,
+    // but it was clicked
+    if (option === clickedOption)
+      // mark it as incorrect
+      return 'red';
+  };
+
   return (
     <section>
       <Stack align='center'>
         <Sprite pokemonForm={pokemonForm} />
         <Group>
           {options.map((option) => (
-            <Button key={option}>{startCase(option)}</Button>
+            <Button
+              key={option}
+              size='lg'
+              onClick={() => !clickedOption && setClickedOption(option)}
+              color={getColor(option)}
+            >
+              {startCase(option)}
+            </Button>
           ))}
         </Group>
       </Stack>
