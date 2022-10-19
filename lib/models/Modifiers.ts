4fbe5bce3279be1c18cf502c4a5e3460e152b780
Itay Raveh <itayitay30@gmail.com>
2022-10-20 01:39:51 +0300

    feat(lib): ✨ add `limit` and `isLimitActive` to `Modifiers`

--- lib/models/Modifiers.ts
+++ lib/models/Modifiers.ts
@@ -2,8 +2,12 @@ import { GenRoman } from 'lib/types/GenRoman';
 
 export interface Modifiers {
   generations: GenRoman[];
+  isLimitActive: boolean;
+  limit: number;
 }
 
 export const modifiersInitialValues: Modifiers = {
   generations: ['I'],
+  isLimitActive: true,
+  limit: 10,
 };
