8e74b6b0fffe749e6ffc2c910c58c4052693ee9f
Itay Raveh <itayitay30@gmail.com>
2022-10-25 23:10:32 +0300

    feat(lib): ✨ add `formCategories` to `models/Modifiers`

--- lib/models/Modifiers.ts
+++ lib/models/Modifiers.ts
@@ -1,13 +1,16 @@
-import { GenRoman } from 'lib/types/GenRoman';
+import type { FormCategory } from 'lib/types/FormCategory';
+import type { GenRoman } from 'lib/types/GenRoman';
 
 export interface Modifiers {
   generations: GenRoman[];
   isLimitActive: boolean;
   limit: number;
+  formCategories: FormCategory[];
 }
 
 export const modifiersInitialValues: Modifiers = {
   generations: ['I'],
   isLimitActive: true,
   limit: 10,
+  formCategories: ['default'],
 };
