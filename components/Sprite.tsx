ac3f950bf003b91635e156eb9d2eae077f43de35
Itay Raveh <itayitay30@gmail.com>
2022-11-05 00:54:32 +0200

    feat(components): ✨ add option for random sprite in `Sprite`

--- components/Sprite.tsx
+++ components/Sprite.tsx
@@ -1,9 +1,100 @@
 import { useModifiers } from 'lib/modifiers/context';
+import sample from 'lodash.sample';
 import Image from 'next/image';
-import type { Pokemon } from 'pokenode-ts';
-import type { FC } from 'react';
+import type { Pokemon, PokemonSprites, VersionSprites } from 'pokenode-ts';
+import { useMemo, type FC } from 'react';
 
 const SPRITE_SIZE = 300 as const;
+
+type ImageRenderingMode = 'pixelated' | 'auto';
+
+interface SpriteData {
+  src: string;
+  rendering: ImageRenderingMode;
+}
+
+const getBestSprite = (sprites: PokemonSprites): SpriteData | null => {
+  const officialArtwork = sprites.other?.['official-artwork'].front_default;
+
+  const home = sprites.other?.home.front_default;
+
+  const sprite = sprites.front_default;
+
+  const best = officialArtwork || home || sprite;
+
+  return best
+    ? { src: best, rendering: best === sprite ? 'pixelated' : 'auto' }
+    : null;
+};
+
+type AnyObj<T> = { [s: string]: T } | ArrayLike<T>;
+
+const getStringsFromObject = <T,>(o: AnyObj<T>): string[] =>
+  Object.values(o).filter(
+    (maybeString) =>
+      maybeString instanceof String || typeof maybeString === 'string'
+  ) as string[];
+
+const toSpriteDataList = <T,>(
+  o: AnyObj<T>,
+  rendering: ImageRenderingMode = 'pixelated'
+): SpriteData[] => getStringsFromObject(o).map((src) => ({ src, rendering }));
+
+type ValueOf<T> = T[keyof T];
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ValueOfNested<T> = T extends any ? ValueOf<T> : never;
+
+type GenerationSprites = ValueOf<VersionSprites>;
+type SubcategorySprites = ValueOfNested<GenerationSprites>;
+
+const getRandomSprite = (
+  sprites: PokemonSprites,
+  whosThatPokemon: boolean
+): SpriteData => {
+  const topLevelSprites = toSpriteDataList({ ...sprites });
+
+  const versionSprites = Object.entries(sprites.versions).flatMap(
+    ([generation, generationSprites]: readonly [string, GenerationSprites]) =>
+      Object.values(generationSprites).flatMap(
+        (subcategorySprites: SubcategorySprites) => {
+          // if `whosThatPokemon` mode is on
+          // and the generation is one of the ones with non-transparent PNGs
+          if (
+            whosThatPokemon &&
+            ['generation-i', 'generation-ii', 'generation-iii'].includes(
+              generation
+            )
+          )
+            // filter out sprites that are not specifically transparent.
+            // this is needed because their silhouettes would just be black squares
+            subcategorySprites = Object.fromEntries(
+              Object.entries(subcategorySprites).filter(([spriteName]) =>
+                spriteName.includes('transparent')
+              )
+            ) as SubcategorySprites;
+
+          return toSpriteDataList({ ...subcategorySprites });
+        }
+      )
+  );
+
+  const allSprites: SpriteData[] = [...topLevelSprites, ...versionSprites];
+
+  const other = sprites.other;
+  console.log('other', other);
+
+  if (other) {
+    allSprites.push(
+      ...[
+        ...toSpriteDataList({ ...other['official-artwork'] }, 'auto'),
+        ...toSpriteDataList({ ...other.home }, 'auto'),
+      ]
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  return sample(allSprites) as SpriteData;
+};
 
 interface SpriteProps {
   pokemon: Pokemon;
@@ -12,20 +103,19 @@ interface SpriteProps {
 const Sprite: FC<SpriteProps> = ({ pokemon }) => {
   const modifiers = useModifiers();
 
-  const officialArtwork =
-    pokemon?.sprites.other?.['official-artwork'].front_default;
+  const sprite = useMemo(
+    () =>
+      modifiers.randomSprite
+        ? getRandomSprite(pokemon.sprites, modifiers.whosThatPokemon)
+        : getBestSprite(pokemon.sprites),
+    [modifiers.randomSprite, modifiers.whosThatPokemon, pokemon.sprites]
+  );
 
-  const home = pokemon?.sprites.other?.home.front_default;
-
-  const sprite = pokemon?.sprites.front_default;
-
-  const best = officialArtwork || home || sprite;
-
-  if (!best) return <section>No sprite for this Pokemon</section>;
+  if (!sprite) return <section>No sprite for this Pokemon</section>;
 
   return (
     <Image
-      src={best}
+      src={sprite.src}
       alt={pokemon.name}
       width={SPRITE_SIZE}
       height={SPRITE_SIZE}
@@ -33,7 +123,7 @@ const Sprite: FC<SpriteProps> = ({ pokemon }) => {
         filter: modifiers.whosThatPokemon
           ? 'brightness(0%)'
           : 'drop-shadow(0px 0px 10px #000)',
-        imageRendering: best === sprite ? 'pixelated' : 'auto',
+        imageRendering: sprite.rendering,
       }}
     />
   );
