97593b666ca926d6d25dae38787bc8c177f7115e
Itay Raveh <itay@raveh.dev>
2026-09-04 13:56:09 +0300

    chore: organize project context and assets

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import logo496Avif from '@/assets/images/logo-496.avif';
 import logo496Webp from '@/assets/images/logo-496.webp';
 import logo992Avif from '@/assets/images/logo-992.avif';
 import logo992Webp from '@/assets/images/logo-992.webp';
-import wordmark from '../../assets/plates/wordmark.png';
+import wordmark from '@/assets/images/wordmark.png';
 
 export const Logo = () => (
   <picture className="logo-picture">
