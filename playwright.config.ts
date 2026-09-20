a19295052d143b3a7f9b1c31db29493142c0ff1f
Itay Raveh <itay@raveh.dev>
2026-09-20 15:04:46 +0300

    test(e2e): limit concurrent browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   testDir: './e2e',
   globalSetup: './e2e/database-setup.ts',
   fullyParallel: true,
+  workers: 2,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
   reporter: process.env.CI ? 'github' : 'list',
